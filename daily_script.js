5a6b5d56fa7afa50fecd8e971566ceb1df33305a
Justin Garner <j.garner828@gmail.com>
2022-01-01 13:35:55 -0500

    fixed daily display button generation to check against day of the week

--- daily_script.js
+++ daily_script.js
@@ -7,13 +7,27 @@ if(!dailyROUTINE) {
 }
 
 function displayDaily() {
+
     // TODO: NEED TO IMPORT MOMENT TO CHECK DAY. CHANGE DAY TO AN INDEX EQUIVALENT VALUE; THEN FILTER THROUGH EACH index of dailyROUTINE TO SEE WHICH TO DISPLAY ACCORDING TO BOOLEAN CHECK
     let today = moment();
     console.log(today._d);
 
+    // Sun = 0
+    // Mon = 1
+    // Tue = 2
+    // Wed = 3
+    // Thu = 4
+    // Fri = 5
+    // Sat = 6
+    var j = 6;
+
+
+// Loops through array checking boolean value of current day vs each exercise to make button
     for(let i = 0; i < dailyROUTINE.length; i++){
-        $('.dailyList').append(`<button class="btn btn-primary m-1" id= "daily_${dailyROUTINE[i].name}"> ${dailyROUTINE[i].name} </button>`);
         console.log(dailyROUTINE[i]);
+         if(dailyROUTINE[i].frequency[j]) {
+            $('.dailyList').append(`<button class="btn btn-primary m-1" id= "daily_${dailyROUTINE[i].name}"> ${dailyROUTINE[i].name} </button>`);
+         }
     }
     
 }
